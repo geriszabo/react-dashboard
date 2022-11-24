f441cf3af54c710c48a369c74ee459aeedff7d21
Jetigeri <szabo.gergo87j@gmail.com>
2022-11-24 20:20:33 +0100

    application started

---
+++ src/data/dataSum.jsx
@@ -0,0 +1,31 @@
+import data from "./data.json"
+
+const { Salesperson: salesperson, Orders: orders, Products: products } = data;
+
+export let dataSum = []
+
+for (let i = 0; i < orders.length; i++) {
+    for (let j = 0; j < salesperson.length; j++) {
+      for (let z = 0; z < products.length; z++) {
+        if (orders[i]["Salesperson ID"] === salesperson[j]["Id"]) {
+          if (products[z]["Product Id"] === orders[i]["Product Id"]) {
+            dataSum.push({
+              orderNumber: i + 1,
+              salespersonName: salesperson[j]["Name"],
+              salespersonId: orders[i]["Salesperson ID"],
+              productId: orders[i]["Product Id"],
+              quantitySold: orders[i]["Number of product sold"],
+              orderStatus: orders[i]["Order status"],
+              account: orders[i]["Account"],
+              date: orders[i]["Order date"],
+              productName: products[z]["Product Name"],
+              unitPrice: products[z]["Unit price"],
+              currency: products[z]["Currency"],
+              orderPrice: products[z]["Unit price"] * orders[i]["Number of product sold"],
+              accountType: orders[i]["Account type"]
+            });
+          }
+        }
+      }
+    }
+  }
